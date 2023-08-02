ac15bbee0eeb29aef295531a3b7b7d95ec96b621
rnade046 <39959339+rnade046@users.noreply.github.com>
2023-08-02 12:01:56 -0400

    update fdr

--- fdr/MainFDR.java
+++ fdr/MainFDR.java
@@ -9,7 +9,7 @@ import java.util.Properties;
 public class MainFDR {
 
 	public static void main(String[] args) throws FileNotFoundException, IOException {
-		
+
 		System.out.println("**Loading parameters file** \n");
 		Properties params = new Properties();
 		params.load(new FileInputStream(args[0]));		
@@ -19,37 +19,37 @@ public class MainFDR {
 
 		//String motifsPrefix = wd + "motifClustering/" + projectName + "_testedDegenMotifClustering_";
 		//String nullMotifsPrefix = wd + "motifClustering/" + projectName + "_testedDegenMotifClustering_";
+		//
+		//		int clusteringMeasure = Integer.parseInt(params.getProperty("clusteringMeasure", "0"));
+		//		double percentThreshold = Double.parseDouble(params.getProperty("percentThreshold", "0.2"));
 
-		int clusteringMeasure = Integer.parseInt(params.getProperty("clusteringMeasure", "0"));
-		double percentThreshold = Double.parseDouble(params.getProperty("percentThreshold", "0.2"));
+		//		String clusteringName = "";
+		//
+		//		switch(clusteringMeasure) {
+		//		case 0: clusteringName = "_TPD";
+		//		break;
+		//		case 1: clusteringName = "_TPPD_p" + percentThreshold;
+		//		break;
+		//		case 2: clusteringName = "_coreTPD_p" + percentThreshold;
+		//		break;
+		//		}
+		//		
+		String motifs_significanceScoresFile = wd +  projectName + "_structure_significantScores.tsv";
+		String nullModel_significanceScoresFile = wd + projectName  + "_structure_significantScores_rand.tsv";
 
-		String clusteringName = "";
-
-		switch(clusteringMeasure) {
-		case 0: clusteringName = "_TPD";
-		break;
-		case 1: clusteringName = "_TPPD_p" + percentThreshold;
-		break;
-		case 2: clusteringName = "_coreTPD_p" + percentThreshold;
-		break;
-		}
-		
-		String motifs_significanceScoresFile = wd + "fdr/" + projectName + clusteringName + "_listOfCalculatedSignificanceScores.tsv";
-		String nullModel_significanceScoresFile = wd + "fdr/" + projectName + "_nullModel" + clusteringName + "_listOfCalculatedSignificanceScores.tsv";
-		
 		SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss");
 		Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-		String fdrOutput = wd + "fdr/" + sdf1.format(timestamp) + "_" + projectName + clusteringName + "_FDRatThresholds_monotonicTransformation.tsv";
+		String fdrOutput = wd + sdf1.format(timestamp) + "_" + projectName + "_FDRatThresholds_monotonicTransformation.tsv";
 		//String significantMotifs = wd + pro
-		
+
 		/* Get list of significance scores */
-		
+
 		/* Compute FDRs between motifs and null model + monotonic transformation */
 		System.out.println("**Initializing FDR calculation**");
 		FdrCalculator fdrCalc = new FdrCalculator(motifs_significanceScoresFile, nullModel_significanceScoresFile);
 		System.out.println("**Computing FDR calculation**");
 		ArrayList<FalseDiscoveryRate> fdr = fdrCalc.computeFdr();
-		
+
 		/* Print information */
 		ExportFdrDetails.exportFDR(fdr, fdrOutput);
 	}
