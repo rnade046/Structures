16ed469a601536de490b13a21386d9c2785d291f
Rachel Nadeau <39959339+rnade046@users.noreply.github.com>
2024-02-07 13:51:05 -0500

    update

--- annotation/reliable/AnalyseRELIABLEmodules.java
+++ annotation/reliable/AnalyseRELIABLEmodules.java
@@ -8,6 +8,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Iterator;
+import java.util.Set;
 
 import org.json.JSONObject;
 
@@ -16,8 +17,8 @@ public class AnalyseRELIABLEmodules {
 
 	public static void main(String[] args) {
 
-		String inputFile = "/Users/rnadeau2/Documents/Structures/BayesPairs/RELIABLE/RELIABLE.json";
-		String outputFile = "/Users/rnadeau2/Documents/Structures/BayesPairs/RELIABLE/reliable-info.tsv";
+		String inputFile = "/Users/Rachel/Documents/rnabayespairing2-master/bayespairing/models/RELIABLE.json";
+		String outputFile = "/Users/Rachel/Modules/reliable-info2.tsv";
 		loadJson(inputFile, outputFile);
 		
 	}
@@ -28,27 +29,28 @@ public class AnalyseRELIABLEmodules {
 			BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(new File(inputFile))));
 			BufferedWriter out = new BufferedWriter(new FileWriter(new File(outputFile)));
 			
-			String line = in.readLine();
-			
-			out.write("module\tmodule_name\tPDBs\n");
+			out.write("Module\t3dAtlasID\tSiblings\tmGraph-nodes\tmGraph-edges\tPDBs\n");
 
-			JSONObject rootObject = new JSONObject(line); // Parse the JSON to a JSONObject
+			JSONObject rootObject = new JSONObject(in.readLine()); // Parse the JSON to a JSONObject
 			Iterator<String> keys = rootObject.keys();
 
 			while(keys.hasNext()){
 
 				/* load modules */
 				String k = keys.next();
-				
+
 				JSONObject m = rootObject.getJSONObject(k); 
 				
-				JSONObject pdbs = m.getJSONObject("PDBs");
+				Set<String> pdbs = m.getJSONObject("PDBs").keySet();
 				String name = m.get("atlas_name").toString();
-			
-				out.write(k + "\t" + name + "\t");
-				for(String pdb : pdbs.keySet()) {
-					out.write(pdb + "|");
-				}
+				String sibs = m.getJSONArray("siblings").toString();
+				
+				JSONObject masterGraph = m.getJSONObject("master_graph");
+				String nodes = masterGraph.get("nodes").toString();
+				String edges = masterGraph.get("edges").toString();
+				
+				out.write(k + "\t" + name + "\t" + sibs.substring(1, sibs.length()-1) + "\t" + nodes.substring(1, nodes.length()-1) +
+						"\t" + edges.substring(1, edges.length()-1) +"\t" + String.join(",", pdbs));
 				
 				out.write("\n");
 				out.flush();
