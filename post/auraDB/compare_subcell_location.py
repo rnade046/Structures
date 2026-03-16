6f52e3dd714335d9bd3e83262bafac44c43da2fb
rnade046 <rnade046@uottawa.ca>
2026-03-16 12:59:25 -0400

    feat: determine human protein atlas co-localization

---
+++ post/auraDB/compare_subcell_location.py
@@ -0,0 +1,66 @@
+import pandas as pd
+
+def load_atlas(atlas_file, factors):
+    fields = ['Gene name', 'Reliability', 'Main location', 'Additional location']
+    atlas = pd.read_csv(atlas_file, sep="\t", usecols=fields)
+
+    # remove rows with Reliability='uncertain'
+    atlas = atlas[atlas['Reliability'] != 'Uncertain'].copy()
+
+    # keep atlas rows for RBP or target proteins
+    genes_to_keep = set(factors["Regulatory factor"]).union(factors["Target Gene"])
+    atlas = atlas[atlas["Gene name"].isin(genes_to_keep)].copy()
+
+    # parse locations
+    atlas["all_locations"] = atlas.apply(parse_locations, axis=1)
+    atlas = atlas.drop(columns=["Main location", "Additional location"])
+    return atlas
+
+
+def parse_locations(row):
+    main = [] if pd.isna(row["Main location"]) else row["Main location"].split(";")
+    add = [] if pd.isna(row["Additional location"]) else row["Additional location"].split(";")
+
+    # combine main and additional locations
+    return {
+        loc.strip()
+        for loc in main + add
+        if loc and loc.strip()
+    }
+
+def load_factors(trans_file):
+    fields = ['Regulatory factor', 'Target Gene']
+    factors = pd.read_csv(trans_file, sep="\t", usecols=fields).drop_duplicates()
+    return factors
+
+
+def assess_shared_locations(factors, atlas, out_file):
+    location_map = atlas.set_index("Gene name")["all_locations"]
+    out = factors.copy()
+    out["rf_locations"] = out["Regulatory factor"].map(location_map)
+    out["tg_locations"] = out["Target Gene"].map(location_map)
+
+    out["matching_locations"] = out.apply(get_overlap, axis=1)
+    out.to_csv(out_file, sep="\t", index=False)
+
+
+def get_overlap(row):
+    if not isinstance(row["rf_locations"], set) or not isinstance(row["tg_locations"], set):
+        return pd.NA
+
+    overlap = row["rf_locations"] & row["tg_locations"]
+    return overlap if overlap else pd.NA
+
+if __name__ == '__main__':
+    wd = "/Users/rnadeau2/Documents/Structures/post/nwTPD2/AuraDB"
+
+    atlas_file = f"{wd}/protein_atlas/subcellular_location.tsv"
+
+    for m in [82, 85, 94, 239]:
+        motif = f"{wd}/output/trans_motif{m}.tsv"
+        out_file = f"{wd}/protein_atlas/atlas_subcell_location_auradb_transfactors_motif{m}.tsv"
+
+        trans = load_factors(motif)
+        atlas = load_atlas(atlas_file, trans)
+        assess_shared_locations(trans, atlas, out_file)
+
