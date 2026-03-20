567d5e80f162ee1fdc428cbc6c4e688f9757a040
rnade046 <rnade046@uottawa.ca>
2026-03-20 10:20:00 -0400

    feat: match cis/trans sites to motif positions

--- post/auraDB/find_auradb_overlap.py
+++ post/auraDB/find_auradb_overlap.py
@@ -21,25 +21,47 @@ def filter_auraDB(auradb_file, hgnc_symbols, fasta_file):
 
     # get genome positions for utr in database
     utr_pos = get_utr_genome_positions_fasta(fasta_file, set(filtered_db["Target_UTR_id"].dropna().unique()))
-
-    coords = (
-        filtered_db["Target_UTR_id"]
-        .map(utr_pos)
-        .str.extract(r"^(chr[^:]+):(\d+)-(\d+)$")
-    )
-    coords.columns = ["chr", "utr_start", "utr_end"]
-
-    filtered_db = pd.concat([filtered_db, coords], axis=1)
-    filtered_db["utr_start"] = filtered_db["utr_start"].astype("Int64")
-    filtered_db["utr_end"] = filtered_db["utr_end"].astype("Int64")
+    filtered_db = filtered_db.merge(utr_pos, on="Target_UTR_id", how="inner")
 
     # get position of binding site from chromosome pos [utr_start/end] +/- [binding site_start/end]
+    minus_mask = (
+            (filtered_db["strand"] == "-") &
+            (filtered_db["Binding site start"] < filtered_db["Target UTR length"])
+    )
+
+    filtered_db.loc[minus_mask, "binding_start"] = (
+            filtered_db.loc[minus_mask, "end_site"] -
+            filtered_db.loc[minus_mask, "Binding site start"]
+    )
+
+    filtered_db.loc[minus_mask, "binding_end"] = (
+            filtered_db.loc[minus_mask, "end_site"] -
+            filtered_db.loc[minus_mask, "Binding site stop"]
+    )
+
+    positive_mask = (
+            (filtered_db["strand"] == "+") &
+            (filtered_db["Binding site start"] < filtered_db["Target UTR length"])
+    )
+
+    filtered_db.loc[positive_mask, "binding_start"] = (
+            filtered_db.loc[positive_mask, "start_site"] +
+            filtered_db.loc[positive_mask, "Binding site start"]
+    )
+
+    filtered_db.loc[positive_mask, "binding_end"] = (
+            filtered_db.loc[positive_mask, "start_site"] +
+            filtered_db.loc[positive_mask, "Binding site stop"]
+    )
+
+    # remove motifs that didn't match in the aura db
+    filtered_db = filtered_db.dropna(subset=["binding_start", "binding_end"])
 
     return filtered_db
 
 
 def get_utr_genome_positions_fasta(fasta_file, utr_ids):
-    utr_to_range = {}
+    records = []
     with open(fasta_file, "r") as fasta:
         for line in fasta:
             line = line.strip()
@@ -50,19 +72,98 @@ def get_utr_genome_positions_fasta(fasta_file, utr_ids):
 
             parts = line.split()
             utr_id = parts[0].lstrip(">")
-            chrom_range = parts[1].split("=", 1)[1]
 
             if utr_id in utr_ids:
-                utr_to_range[utr_id] = chrom_range
-    return utr_to_range
+                chrom_range = None
+                strand = None
 
+                for field in parts[1:]:
+                    if "=" not in field:
+                        continue
+
+                    key, value = field.split("=", 1)
+
+                    if key == "range":
+                        chrom_range = value
+                    elif key == "strand":
+                        strand = value
+                chrom, pos = chrom_range.split(":", 1)
+                start_site, end_site = pos.split("-", 1)
+
+                records.append({
+                    "Target_UTR_id": utr_id,
+                    "chr": chrom,
+                    "start_site": int(start_site),
+                    "end_site": int(end_site),
+                    "strand": strand,
+                })
+
+    return pd.DataFrame(records)
+
+
+def match_positions(bed_file, aura_db, out_file, clean):
+    motif = pd.read_csv(bed_file, sep="\t", header=None)
+    motif.columns = ["chr", "motif_start", "motif_end", "pos", "idx"]
+
+    matches = motif.merge(aura_db, on="chr", how="inner")
+
+    matches = matches[
+        (matches["motif_start"] < matches["binding_end"]) &
+        (matches["motif_end"] > matches["binding_start"])
+        ]
+
+    if not matches.empty:
+        if not clean:
+            matches.to_csv(out_file, sep="\t", index=False)
+        else :
+            matches2 = clean_matches(matches)
+            matches2[["chr", "motif_start","motif_end",  "Regulatory factor",
+                "Target Gene", "Target_UTR_ids",   "binding_start", "binding_end"]
+            ].to_csv(out_file, sep="\t", index=False)
+
+
+def clean_matches(match_df):
+    summary_df = match_df[
+        [
+            "chr",
+            "motif_start",
+            "motif_end",
+            "Regulatory factor",
+            "Target Gene",
+            "Target_UTR_id",
+            "strand",
+            "binding_start",
+            "binding_end",
+        ]
+    ].copy()
+
+    summary_df = (
+        summary_df
+        .groupby(
+            ["motif_start", "motif_end", "binding_start", "binding_end"],
+            as_index=False,
+            dropna=False,
+        )
+        .agg({
+            "chr": "first",
+            "Regulatory factor": "first",
+            "Target Gene": "first",
+            "strand": "first",
+            "Target_UTR_id": lambda x: ", ".join(sorted(set(x.dropna()))),
+        })
+        .rename(columns={"Target_UTR_id": "Target_UTR_ids"})
+    )
+
+    return summary_df
 
 if __name__ == '__main__':
 
     wd = "/Users/rnadeau2/Documents/Structures/post/nwTPD2/AuraDB"
     biomart_file = f"{wd}/input/BiomaRt_corrNet2_refSeq_UCSD.tsv"
     cis_auradb_file = f"{wd}/input/AURAlight_cis-elements.txt"
+    trans_auradb_file = f"{wd}/input/AURAlight_trans-factors.txt"
     fasta_file = f"{wd}/input/UTR_hg19.fasta"
+
 
     # load cis / trans db : limit to 3'UTRs associated to target genes network (w/ BiomaRt file)
     id_mapping = load_biomart_mapping(biomart_file)
@@ -70,5 +171,13 @@ if __name__ == '__main__':
 
     # obtain chromosome positions for target sites (hg19.fasta)
     cisdb = filter_auraDB(cis_auradb_file, unique_hgnc, fasta_file)
+    transdb = filter_auraDB(trans_auradb_file, unique_hgnc, fasta_file)
 
-    # check for matching chromosome regions
+    for i in [82, 85, 94, 239]:
+        bed_file = f"{wd}/bed/hglft_genome_{i}.bed"
+        cis_out_file = f"{wd}/output/cisdb_motif{i}.tsv"
+        trans_out_file = f"{wd}/output/trans_motif{i}.tsv"
+
+        # check for matching chromosome regions
+        match_positions(bed_file, cisdb, cis_out_file, False)
+        match_positions(bed_file, transdb, trans_out_file, True)
